c3288d397f1301d30a15a54288ae47fb307edfba
deoesor <RulianValeriy@yandex.ru>
2024-10-19 14:59:53 +0300

    lesson 2: working with types, functions, methods

--- cmd/cat-store/main.go
+++ cmd/cat-store/main.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 
 	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/caarlos0/env/v11"
 )
 
 func main() {
+	var config Config
+	err := config.Load()
+	if err != nil {
+		fmt.Printf("error while parsing config: %v", err)
+		return
+	}
 
 	language := "go"
 
-	fmt.Printf("Hello %v", language)
+	fmt.Printf("Hello %v, current environemnt %v", language, config.Environment)
 
+}
+
+type Config struct {
+	Environment string `env:"ENVIRONMENT"`
+}
+
+func (config *Config) Load() error {
+	return env.Parse(config)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.22.4
 replace github.com/DeoEsor/MAI-course-backend-go => ./
 
 require (
-	github.com/caarlos0/env/v11 v11.2.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/caarlos0/env/v11 v11.2.2
+	github.com/joho/godotenv v1.5.1
 )
